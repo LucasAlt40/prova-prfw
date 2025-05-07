fb969ba2091548a4bc82f94bed8fc0d6ffd651ae
Eduardo Pires <epc4129@gmail.com>
2025-05-07 20:56:01 -0300

    populando tipo veiculos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
 
 INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('CARRO', 2,10.50);
+INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('MOTO', 1,5.50);
+INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('CAMINHAO', 4,25.50);
+
+insert
 
