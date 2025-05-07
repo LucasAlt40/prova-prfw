9fff5bda46b5b253d1e140d7ff02c90c80b7089e
LucasAlt40 <lucas.alt40@gmail.com>
2025-05-07 20:58:51 -0300

    security

--- src/main/java/edu/ifsp/provaspring/config/SecurityConfig.java
+++ src/main/java/edu/ifsp/provaspring/config/SecurityConfig.java
@@ -1,23 +1,39 @@
 package edu.ifsp.provaspring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(
-                User.withUsername("joao")
-                        .password("{noop}abc")
-                        .roles("USER").build());
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
         return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+                )
+                .headers(headers -> headers.frameOptions().disable())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
+        return http.build();
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,11 @@
 
 INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('CARRO', 2,10.50);
 
+delete from users;
+insert into users (username, password, enabled) values ('doug', '{noop}paty', true);
+insert into users (username, password, enabled) values ('tintin', '{noop}milu', true);
+
+delete from authorities;
+insert into authorities (username, authority) values ('doug', 'ROLE_USER');
+insert into authorities (username, authority) values ('tintin', 'ROLE_USER');
+insert into authorities (username, authority) values ('tintin', 'ROLE_ADMIN');
