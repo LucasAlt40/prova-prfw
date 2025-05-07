e182ae064ae9cde4a5aaad1780f86fe3f34e44f2
LucasAlt40 <lucas.alt40@gmail.com>
2025-05-07 19:29:36 -0300

    first commit

---
+++ src/main/java/edu/ifsp/provaspring/ProvaSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.ifsp.provaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaSpringApplication.class, args);
+    }
+
+}
