b8b5473edb426f03301d0d4acec067918b3237f1
Eduardo Pires <epc4129@gmail.com>
2025-05-07 21:03:35 -0300

    populando tipo veiculos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,13 +3,14 @@ INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('CARRO', 
 INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('MOTO', 1,5.50);
 INSERT INTO tipo_veiculos (tipo, quantidade_vagas, preco_hora) values ('CAMINHAO', 4,25.50);
 
-insert INTO veiculos (placa, modelo, cor, tipo_id) values ('XPTO', "GOL QUADRADO", 'BRANCO', 1)
+insert INTO veiculos (placa, modelo, cor, tipo_id) values ('XPTO', 'GOL QUADRADO', 'BRANCO', 1)
+insert INTO veiculos (placa, modelo, cor, tipo_id) values ('PXTO', 'CG 150', 'PRETO', 2)
+insert INTO veiculos (placa, modelo, cor, tipo_id) values ('HPTO', 'SCANIA 3000', 'VERDE', 3)
+
 
 delete from users;
-insert into users (username, password, enabled) values ('doug', '{noop}paty', true);
-insert into users (username, password, enabled) values ('tintin', '{noop}milu', true);
+insert into users (username, password, enabled) values ('lucas', '{noop}123', true);
 
 delete from authorities;
-insert into authorities (username, authority) values ('doug', 'ROLE_USER');
-insert into authorities (username, authority) values ('tintin', 'ROLE_USER');
-insert into authorities (username, authority) values ('tintin', 'ROLE_ADMIN');
+insert into authorities (username, authority) values ('lucas', 'ROLE_USER');
+insert into authorities (username, authority) values ('lucas', 'ROLE_ADMIN');
