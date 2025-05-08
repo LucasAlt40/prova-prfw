2e6e0676b49794b0e887ee5d4fc513f1b9787a45
LucasAlt40 <lucas.alt40@gmail.com>
2025-05-07 21:15:42 -0300

    security

--- src/main/java/edu/ifsp/provaspring/config/SecurityConfig.java
+++ src/main/java/edu/ifsp/provaspring/config/SecurityConfig.java
@@ -28,9 +28,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
+                .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
         return http.build();
     }
 }
